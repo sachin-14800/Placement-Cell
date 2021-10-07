24d48f5183e272ca0c1c8bf8a0fd88a13ad7dd61
sachin-14800 <sachin.14800@gmail.com>
2021-10-07 19:35:39 +0530

    changes in the course creation

--- controllers/course_controller.js
+++ controllers/course_controller.js
@@ -13,10 +13,14 @@ module.exports.addCourse=async function(req,res)
 }
 module.exports.createCourse=async function(req,res){
     let student=await Student.findById(req.params.id);
-    let course=await Course.create({name:req.body.name,startDate:req.body.date,completionScore:req.body.score});
-    student.courses.push(course.id);
-    course.students.push(student.id);
-    student.save();
-    course.save();
+    let course=await Course.find({name:req.body.name,startDate:req.body.date});
+    if(!course){
+        course=await Course.create({name:req.body.name,startDate:req.body.date,completionScore:req.body.score});
+        student.courses.push(course.id);
+        course.students.push(student.id);
+        student.save();
+        course.save();
+    }
+   
     return res.redirect('/student/profile/'+student.id);
 }
